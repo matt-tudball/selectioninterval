e1e9a275ca057dd5a63e6f478fb5d16d91c7c578
matt-tudball <matt.tudball@bristol.ac.uk>
2021-10-05 20:30:10 +0100

    Minor changes

--- simulation/applied_example.R
+++ simulation/applied_example.R
@@ -2,10 +2,10 @@ rm(list=ls())
 library(devtools); library(foreign); library(ggplot2)
 setwd("C:/Users/ow18301/OneDrive - University of Bristol/MyFiles-Migrated/Documents/IEASBOS_FILES_")
 
-#p <- c(0.02, 0.2, 2) # 1
+p <- c(0.02, 0.2, 2) # 1
 #p <- c(0.01, 0.5, 2) # 2
 #p <- c(0.02, 0.2, 2.5) # 3
-p <- c(0.01, 0.5, 2.5) # 4
+#p <- c(0.01, 0.5, 2.5) # 4
 #p <- c(0.02, 0.2, 1.75) # 5
 #p <- c(0.01, 0.5, 1.75) # 6
 
@@ -56,7 +56,7 @@ mycons4 <- list(
 #)
 
 # Intervals
-out0 <- selection_bound(y=y, x=x, z=z, w=w, L0l=p[1], L0u=p[2], L1=p[3], cons=NULL, opts=list(maxeval=1e5))
+out0 <- selection_bound(y=y, x=x, z=z, w=w, L0l=p[1], L0u=p[2], L1=p[3], cons=NULL)
 
 out1 <- selection_bound(y=y, x=x, z=z, w=w, L0l=p[1], L0u=p[2], L1=p[3], cons=mycons1)
 
@@ -70,8 +70,8 @@ out4 <- selection_bound(y=y, x=x, z=z, w=w, L0l=p[1], L0u=p[2], L1=p[3], cons=my
 
 # Statistics implied by weights
 u <- as.matrix(w); u <- apply(u, 2, function(v) (v - mean(v))/sd(v))
-inv_wgt_min <- 1+exp(as.matrix(-cbind(1,u))%*%out4$theta_min)
-inv_wgt_max <- 1+exp(as.matrix(-cbind(1,u))%*%out4$theta_max)
+inv_wgt_min <- 1+exp(as.matrix(-cbind(1,u))%*%out0$theta_min)
+inv_wgt_max <- 1+exp(as.matrix(-cbind(1,u))%*%out0$theta_max)
 
 # For min
 print(paste("Population mean of males:", round(weighted.mean(x=w$w_sex, w=inv_wgt_min),4)))
@@ -90,7 +90,17 @@ main <- data.frame(type=c("0","1","2","3","4"),
                    interval_lower=c(out0$interval[1], out1$interval[1], out2$interval[1], out3$interval[1], out4$interval[1]),
                    interval_upper=c(out0$interval[2], out1$interval[2], out2$interval[2], out3$interval[2], out4$interval[2]))
 
-saveRDS(main, file="SIMULATIONS/DATA/applied_example_4.rds")
+
+main <- readRDS("~/IEASBOS_FILES_/SIMULATIONS/DATA/applied_example_1.rds")
+
+# Add raw estimate
+main <- rbind(main, c("5",0.08, 0.28, 0.17, 0.19))
+main$ci_lower <- as.numeric(main$ci_lower)
+main$ci_upper <- as.numeric(main$ci_upper)
+main$interval_lower <- as.numeric(main$interval_lower)
+main$interval_upper <- as.numeric(main$interval_upper)
+
+saveRDS(main, file="SIMULATIONS/DATA/applied_example_1.rds")
 
 plot <- ggplot(main, aes(type)) +
   geom_hline(aes(yintercept=0), color = "grey", linetype="dashed") +
@@ -108,9 +118,9 @@ plot <- ggplot(main, aes(type)) +
   ) +
   theme(axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank()) +
   ylab("Intervals for the effect estimate") +
-  scale_colour_discrete(name="Constraints", labels=c("None","1","2","3","4")) +
+  scale_colour_discrete(name="Constraints", labels=c("None","1","2","3","4","Point")) +
   coord_flip()
 
 plot
-ggsave(filename=paste("FIGURES/applied_example_4",type=".pdf",sep=""),plot=plot)
+ggsave(filename=paste("FIGURES/applied_example_1",type=".pdf",sep=""),plot=plot)
 
